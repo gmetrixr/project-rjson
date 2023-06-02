faa0311b434ab1cfc3d3c8461217df686a1266be
Ratan Khalsa <ratan.khalsa@gmetri.com>
2023-06-02 06:06:57 +0000

    adding topic_yo to topic types

--- src/r/recordTypes/Topic.ts
+++ src/r/recordTypes/Topic.ts
@@ -1,11 +1,13 @@
 export enum TopicProperty {
   topic_position = "topic_position",
   topic_resolved = "topic_resolved",
+  topic_yp = "topic_yp",
   topic_scene_id = "topic_scene_id",
 }
 
 export const topicPropertyDefaults: Record<TopicProperty, unknown> = {
   [TopicProperty.topic_position]: [0, 0, 0], // [x, y, z] world coordinates
   [TopicProperty.topic_resolved]: false,
+  [TopicProperty.topic_yp]: [0, 0],
   [TopicProperty.topic_scene_id]: -1
 }
