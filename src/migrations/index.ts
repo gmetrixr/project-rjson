a219ec18024e185d265500405898a44d34cf2768
Siddharth <siddharth.pokharna@gmetri.com>
2023-07-20 14:14:23 +0530

    [add] export for 

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,10 +1,12 @@
 import { createNewDeployment, migrateDeployment, getHighestDeploymentVersion } from "./deployment";
 import { migrateDiscussion } from "./discussion";
+import { migrateGlobalVarsDefitions } from "./globalVariables";
 import { createNewProject, migrateProject, getHighestProjectVersion, runHealthCheckMigrations } from "./project";
 
 const migrations = {
   createNewDeployment, migrateDeployment, getHighestDeploymentVersion,
   migrateDiscussion,
+  migrateGlobalVarsDefitions,
   createNewProject, migrateProject, getHighestProjectVersion, runHealthCheckMigrations
 }
 
