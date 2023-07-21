97565aad04f3c65d8dd4198f4944442a85b961af
Siddharth <siddharth.pokharna@gmetri.com>
2023-07-21 15:29:14 +0530

    [fix] create new project function

--- src/migrations/project/newproject-migration-commands/create_first_scene.ts
+++ src/migrations/project/newproject-migration-commands/create_first_scene.ts
@@ -1,9 +1,6 @@
 import { createRecord, RecordNode, RT, sn } from "../../../r";
 import { IOrder } from "../../IOrder";
-import { ProjectUtils } from "../../../r/recordFactories/ProjectFactory";
-import { jsUtils } from "@gmetrixr/gdash";
-
-const { generateIdV2 } = jsUtils;
+import { ProjectFactory, ProjectUtils } from "../../../r/recordFactories/ProjectFactory";
 
 /**
  * Adds predefined variables to the project json
@@ -13,8 +10,11 @@ class Migration implements IOrder {
     const pJson = projectJson as RecordNode<RT.project>;
     const scene = createRecord(RT.scene);
     scene.props.scene_type = sn.SceneType.six_dof;
-    const sceneId = generateIdV2();
-    ProjectUtils.setupNewScene(pJson, { id: sceneId, record: scene });
+    const projectF = new ProjectFactory(pJson);
+    const sceneIdAndRecord = projectF.addRecord({ record: scene });
+    if (sceneIdAndRecord) {
+      ProjectUtils.setupNewScene(pJson, sceneIdAndRecord);
+    }
   }
 }
 
