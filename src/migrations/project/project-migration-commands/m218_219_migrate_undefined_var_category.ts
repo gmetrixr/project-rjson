223afa4895eb578305956064e7f29822581f5c0b
Vivek <vivek@gmetri.com>
2024-08-13 16:56:30 +0530

    added migration for var_category

---
+++ src/migrations/project/project-migration-commands/m218_219_migrate_undefined_var_category.ts
@@ -0,0 +1,29 @@
+import { r, RF } from "../../../index.js";
+import { VarCategory } from "../../../r/definitions/variables/VariableTypes.js";
+import { RT, rtp } from "../../../r/R/index.js";
+import { IOrder } from "../../IOrder.js";
+
+class Migration implements IOrder {
+  execute(projectJson: any) {
+    return migrateProject(projectJson);
+  }
+}
+
+const migrateProject = (json: any) => {
+  const pf = r.project(json);
+  const variables = pf.getSortedRecordEntries(RT.variable);
+  
+  for(const [_vId, variable] of variables) {
+    const varF = new RF.RecordFactory(variable);
+    const varCategory = varF.get(rtp.variable.var_category);
+    if(varCategory === undefined) {
+      varF.set(rtp.variable.var_category, VarCategory.user_defined);
+    }
+  }
+
+  pf.set(rtp.project.version, 219);
+  return json;
+}
+
+const migration = new Migration();
+export default migration;
